ff03278837809718a4a79191718a9fa368429a69
jishnu <107295376+kannanjk@users.noreply.github.com>
2024-02-16 10:17:24 +0530

    mergSort1

--- JavaScript/Sorting/MergeSort.js
+++ JavaScript/Sorting/MergeSort.js
@@ -1,12 +1,9 @@
-const mergSort = (arr1, arr2) => {
+const merg = (arr1, arr2) => {
     var i = 0
     var j = 0
     var result = []
-    while (i < arr1.length || j < arr2.length) {
-        if (i === arr1.length) {
-            result.push(arr2[j])
-            j++
-        } else if (j === arr2.length || arr1[i] <= arr2[j]) {
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
             result.push(arr1[i])
             i++
         } else {
@@ -14,7 +11,23 @@ const mergSort = (arr1, arr2) => {
             j++
         }
     }
+    while (i < arr1.length) {
+        result.push(arr1[i])
+        i++
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j])
+        j++
+    }
     return result
 }
 
-console.log(mergSort([1, 44, 6], [84, 7, 5]));
+const mergSort = (arr) => {
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    let left = mergSort(arr.slice(0, mid))
+    let right = mergSort(arr.slice(mid))
+    return merg(left, right)
+}
+
+console.log(mergSort([1, 44, 6, 84, 7, 5]));
